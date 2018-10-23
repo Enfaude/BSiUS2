a10b029c9397ad4efe0266634a8be95662239a83
EnfaudePC <adammaciak96@gmail.com>
2018-10-23 20:14:19 +0200

    działająca wersja podstawowa z inwersją bitów

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,53 +4,44 @@ import java.util.*;
 
 
 public class Main {
-    static char[] basicEncode(char[] text, int key)
+    private static char[] basicEncode(char[] text, int key)
     {
-        char[] tempEncode = text;
-        System.out.println("Kody ASCII kodowania:");
-        for (int i = 0; i < tempEncode.length; i++)
+        for (int i = 0; i < text.length; i++)
         {
-            tempEncode[i] = (char)(tempEncode[i] + key);
-//            System.out.println((int)tempEncode[i]);
-            tempEncode[i] = (char)(255 - (int)tempEncode[i]);
-//            System.out.println("po inwersji bitów: " + tempEncode[i]);
-//zmiana
+            text[i] = (char)(text[i] + key);
+            text[i] = (char)(255 - (int)text[i]);
         }
-        System.out.println("Tekst zaszyfrowany: "+ tempEncode);
-        return tempEncode;
+        System.out.println("Tekst zaszyfrowany: ");
+        for (char letter : text) {
+            System.out.print(letter);
+        }
+        System.out.println(" ");
+        return text;
     }
 
-    static char[] basicDecode(char[] text, int key)
+    private static char[] basicDecode(char[] text, int key)
     {
-        char[] tempDecode = text;
-        System.out.println("Kody ASCII dekodowania:");
-        for (int i = 0; i < tempDecode.length; i++)
+        for (int i = 0; i < text.length; i++)
         {
-            tempDecode[i] = (char)(255 - tempDecode[i]);
-//            System.out.println("po inwersji wtórnej: " + (int)tempDecode[i]);
-            tempDecode[i] = (char)((int)tempDecode[i] - key);
-//            System.out.println((int)tempDecode[i]);
+            text[i] = (char)(255 - text[i]);
+            text[i] = (char)((int)text[i] - key);
         }
-        System.out.println("Tekst odszyfrowany: "+ tempDecode);
-        return tempDecode;
+        System.out.println("Tekst zdeszyfrowany: ");
+        for (char letter : text) {
+            System.out.print(letter);
+        }
+        System.out.println(" ");
+        return text;
     }
-
-
 
     public static void main(String[] args) {
-        System.out.println();
         Scanner sc = new Scanner(System.in);
         System.out.println("Wprowadź tekst do zaszyfrowania");
-        String tekst = sc.nextLine();
+        char[] tekst = sc.nextLine().toCharArray();
         System.out.println("Wprowadź klucz szyfrowania");
-
         int klucz = sc.nextInt();
 
-
-        char[] encoded = basicEncode(tekst.toCharArray(), klucz);
+        char[] encoded = basicEncode(tekst, klucz);
         char[] decoded = basicDecode(encoded, klucz);
-
-        System.out.println(encoded);
-        System.out.println(decoded);
     }
 }
